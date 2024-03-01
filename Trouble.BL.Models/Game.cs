255888baba69ef2c11f3d12c64ee1e9827000fd8
Luke Oskey <300077578@fvtc.edu>
2024-03-01 09:30:11 -0600

    Added BL Models #46

---
+++ Trouble.BL.Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trouble.BL.Models
+{
+    public class Game
+    {
+        public Guid Id { get; set; }
+
+        [DisplayName("Turn Number")]
+        public int TurnNum { get; set; }
+
+        [DisplayName("Game Name")]
+        public string GameName { get; set; }
+
+        [DisplayName("Game Date")]
+        public DateTime GameDate { get; set; }
+
+    }
+}
---
+++ Trouble.BL.Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trouble.BL.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+    }
+}
