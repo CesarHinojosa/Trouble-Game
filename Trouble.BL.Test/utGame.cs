272c4687ad6ba92ff5894a035adf935e48d4ac51
Cesar Hinojosa Bautista <300077836@fvtc.edu>
2024-03-06 14:43:02 -0600

    Finished utGame and created class utPieceGame and utUserGame

--- Trouble.BL.Test/utGame.cs
+++ Trouble.BL.Test/utGame.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Trouble.Bl.Test;
+using Trouble.BL.Models;
 
 namespace Trouble.BL.Test
 {
@@ -17,6 +18,45 @@ namespace Trouble.BL.Test
             int expected = 2;
 
             Assert.AreEqual(expected, game.Count);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            Game game = new Game
+            {
+                GameDate = DateTime.Now,
+                TurnNum = 88,
+                GameName = "Insert Game Name BL"
+            };
+
+            int result = new GameManager(options).Insert(game, true);
+            Assert.IsTrue(result > 0);
+        }
+
+        [TestMethod]
+        public void UpdateTest()
+        {
+            Game game = new GameManager(options).Load().FirstOrDefault();
+            game.TurnNum = 99;
+            game.GameName = "Updated Game Name BL";
+
+            Assert.IsTrue(new GameManager(options).Update(game, true) > 0);
+        }
+
+        [TestMethod]
+        public void DeleteTest() 
+        {
+            Game game = new GameManager(options).Load().FirstOrDefault(x => x.GameName == "Game2");
+
+            Assert.IsTrue(new GameManager(options).Delete(game.Id, true) > 0);
+        }
+
+        [TestMethod]
+        public void LoadByIdTest()
+        {
+            Game game = new GameManager(options).Load().FirstOrDefault();
+            Assert.AreEqual(new GameManager(options).LoadById(game.Id).Id, game.Id);
         }
     }
 }
--- Trouble.PL.Test/utGame.cs
+++ Trouble.PL.Test/utGame.cs
@@ -61,5 +61,7 @@ namespace Trouble.PL.Test
                 
             }
         }
+
+        
     }
 }
