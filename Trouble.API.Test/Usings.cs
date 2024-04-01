61b7c6c67458342d509988d7ef0fa488a947617d
Cesar Hinojosa Bautista <300077836@fvtc.edu>
2024-04-01 13:45:00 -0500

    Added Game, Piece, User, PieceGame, UserGame Controllers. Added Trouble Hub. Added utBase APITest

---
+++ Trouble.API.Test/Usings.cs
@@ -0,0 +1,6 @@
+﻿global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Microsoft.VisualStudio.TestPlatform.TestHost;
+global using Newtonsoft.Json;
+global using Newtonsoft.Json.Linq;
+global using System.Reflection;
+
--- Trouble.API/Program.cs
+++ Trouble.API/Program.cs
@@ -5,58 +5,64 @@ using Serilog.Ui.MsSqlServerProvider;
 using Serilog.Ui.Web;
 using System.Reflection;
 
-//Need to work on API
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-//THIS IS FOR LOGGING PLUMBING------------------------------------
-string connection = builder.Configuration.GetConnectionString("DatabaseConnection");
-
-
-builder.Services.AddSerilogUi(options =>
+public class Program
 {
-    options.UseSqlServer(connection, "Logs");
-});
+    private static void Main(string[] args)
+    {
+        //Need to work on API
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        //THIS IS FOR LOGGING PLUMBING------------------------------------
+        string connection = builder.Configuration.GetConnectionString("DatabaseConnection");
 
 
-var configsettings = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
+        builder.Services.AddSerilogUi(options =>
+        {
+            options.UseSqlServer(connection, "Logs");
+        });
 
-Log.Logger = new LoggerConfiguration()
-    .ReadFrom.Configuration(configsettings)
-    .CreateLogger();
 
-builder.Services
-    .AddLogging(c => c.AddDebug())
-    .AddLogging(c => c.AddSerilog())
-    .AddLogging(c => c.AddEventLog())
-    .AddLogging(c => c.AddConsole());
+        var configsettings = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
 
-var app = builder.Build();
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configsettings)
+            .CreateLogger();
 
-app.UseSerilogUi(options => { options.RoutePrefix = "logs"; });
+        builder.Services
+            .AddLogging(c => c.AddDebug())
+            .AddLogging(c => c.AddSerilog())
+            .AddLogging(c => c.AddEventLog())
+            .AddLogging(c => c.AddConsole());
 
-//---------------------------------------------------------------- Logging from plumbing
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        app.UseSerilogUi(options => { options.RoutePrefix = "logs"; });
+
+        //---------------------------------------------------------------- Logging from plumbing
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-
-app.Run();
