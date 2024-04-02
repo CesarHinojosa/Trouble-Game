05ee3b31f84eeefd0e6f637a79b716a96e8f0fb0
Cesar Hinojosa Bautista <300077836@fvtc.edu>
2024-04-02 16:57:46 -0500

    Added API Test

---
+++ Trouble.API.Test/utUser.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SqlServer.Server;
+using Trouble.BL.Models;
+
+namespace Trouble.API.Test
+{
+    [TestClass]
+    public class utUser : utBase
+    {
+        //[TestMethod]
+        //public async Task LoadTestAsync()
+        //{
+        //    await base.LoadTestAsync<User>();
+        //}
+
+        [TestMethod]
+        public async Task InsertTestAsync()
+        {
+            User user = new User { Username = "Test User" };
+            await base.InsertTestAsync<User>(user);
+
+        }
+
+        //[TestMethod]
+        //public async Task DeleteTestAsync()
+        //{
+        //    await base.DeleteTestAsync1<User>(new KeyValuePair<string, string>("Username", "Other"));
+        //}
+
+        //[TestMethod]
+        //public async Task LoadByIdTestAsync()
+        //{
+        //    await base.LoadByIdTestAsync<User>(new KeyValuePair<string, string>("Username", "Other"));
+        //}
+
+        //[TestMethod]
+        //public async Task UpdateTestAsync()
+        //{
+        //    User user = new User { Username = "Test User" };
+        //    await base.UpdateTestAsync<User>(new KeyValuePair<string, string>("Username", "Other"), user);
+
+        //}
+    } 
+}
---
+++ Trouble.API.Test/utUserGame.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.SqlServer.Server;
+using Trouble.BL.Models;
+
+namespace Trouble.API.Test
+{
+    [TestClass]
+    public class utUserGame : utBase
+    {
+        //Insert Update Delete
+
+    }
+}
